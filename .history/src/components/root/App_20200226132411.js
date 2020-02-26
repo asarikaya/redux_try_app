2a6e121a78052bc1f6376144c5c40192b86b4e0f
asarikaya <a.sarikaya@ymail.com>
2020-02-26 13:31:17 +0300

    Ana şablon hazırlandı

--- .history/src/components/root/App_20200226132411.js
+++ .history/src/components/root/App_20200226132411.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Container } from 'reactstrap';
 import Navi from "../navi/Navi"
-import Dashboard from './Dashboard';
 
 function App() {
   return (
     <Container>
-      <Navi/>
-      <Dashboard/>
+      <Navi></Navi>
     </Container>
   );
 }
---
+++ .history/src/components/root/Dashboard_20200226132034.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import { Row, Col } from 'reactstrap'
+
+export default class Dashboard extends Component {
+    render() {
+        return (
+            <div>
+                <Row>
+                    <Col xs="3">
+                        <CategoryList/>
+                    </Col>
+                    <Col xs="9">
+                        <ProductList/>
+                    </Col>
+                </Row>
+            </div>
+        )
+    }
+}
